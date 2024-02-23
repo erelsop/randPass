0c62a7fd8603cc63b08103b6ab31461859ffc687
Ethan Elsop <eelsop@getuwired.com>
2024-02-23 02:21:55 -0500

    Update package.json & rollup.config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,23 +1,11 @@
-import typescript from '@rollup/plugin-typescript';
-import terser  from '@rollup/plugin-terser';
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: './src/index.ts',
-  output: [
-    {
-      file: './dist/index.js',
-      format: 'es',
-    },
-  ],
-  plugins: [
-    typescript(),
-    terser(),
-  ],
-  external: [
-    'readline-sync',
-    'crypto',
-    'fs',
-    'inquirer',
-    'chalk',
-  ]
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.mjs",
+    format: "es",
+  },
+  plugins: [typescript()],
+  external: ["readline-sync", "crypto", "fs", "inquirer", "chalk"],
 };
